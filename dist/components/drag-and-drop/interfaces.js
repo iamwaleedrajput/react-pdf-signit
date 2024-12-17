56ecba722547ec10fe3b46950701a1e1f20344fa
iamwaleedrajput <waleedrajput7453@gmail.com>
2024-12-17 12:36:43 +0500

    dist removed

--- dist/components/drag-and-drop/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
